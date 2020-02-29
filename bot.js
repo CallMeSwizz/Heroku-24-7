8e71e0e3a770e68765251e5cc29311d6758d93e7
CallMeSwizz <47117767+CallMeSwizz@users.noreply.github.com>
2020-02-29 18:19:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-{
-"prefix": "d!"
-}
-
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   client.user.setActivity(`Serving ${client.guilds.size} server`);
