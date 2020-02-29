4cd812b8c1e1fd95a146128bd85d97a715343a33
CallMeSwizz <47117767+CallMeSwizz@users.noreply.github.com>
2020-02-29 17:13:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Serving ${client.guilds.size} server`);
 });
 
 client.on('message', message => {
@@ -12,6 +12,24 @@ client.on('message', message => {
     	message.channel.send('Pong!')
   	}
 });
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
