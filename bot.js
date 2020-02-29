040dd1648079f9405564aa12597b39d57000c258
CallMeSwizz <47117767+CallMeSwizz@users.noreply.github.com>
2020-02-29 18:27:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} server`);
-});
-
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('Pong!')
